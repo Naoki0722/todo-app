682d880068bc22e73357d8e0d6cb9abf2930a119
Naoki0722 <mattu.nao722@gmail.com>
2022-05-23 23:35:30 +0900

    add material ui & emotion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
+import { Button, Stack } from '@mui/material'
 import { VFC } from 'react'
 import { Link } from 'react-router-dom'
+import { css } from "@emotion/react";
+
+const style = css`
+  background-color: blue;
+  &:hover {
+    background-color: red;
+  }
+`;
 
 const App: VFC = () => (
   <div className="App">
@@ -14,6 +23,13 @@ const App: VFC = () => (
         </li>
       </ul>
     </nav>
+    <h2>サンプルスタイル</h2>
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <Button variant="contained" css={style}>ボタン</Button>
+    </Stack>
   </div>
 )
 
